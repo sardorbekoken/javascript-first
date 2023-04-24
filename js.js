7dc659df6ea434441449fc0cef52d446936f61b4
sardorbek <sotvoldiyevsardorbek77@gmail.com>
2023-04-24 09:18:14 +0500

    add first

---
+++ js.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//rest operatorlar
+
+function myskills(lang, cms,  ...rest) {
+
+    console.log(lang, cms, rest);
+
+}
+
+myskills("javascript", "java", "node js", "html", "css")
+
+let salom = [
+    {
+        "name": "Mahmut",
+        "age": 20,
+        "city": "İstanbul"
+    },
+
+]
